ed65f570899c38808ce5624fe323b67817639cd4
HugoSchtr <hugo.scheithauer@inria.fr>
2024-02-21 20:54:45 +0100

    working on sentence segmentation

--- dev/tei2conll_all.py
+++ dev/tei2conll_all.py
@@ -1,7 +1,10 @@
 import os
+import re
 
 from bs4 import BeautifulSoup
 import spacy
+from spacy.attrs import ORTH, NORM
+import pandas as pd
 
 # TODO: refactor
 
@@ -9,8 +12,12 @@ DISABLED = [
     "ner", "tok2vec", "tagger", "parser", "attribute_ruler", "lemmatizer"]
 
 # Variables to change depending on corpus
-corpus_directory = '../../lang_subset2_EN_DE/de'
+corpus_directory = './tei'
 nlp = spacy.load("xx_sent_ud_sm")
+nlp.tokenizer.add_special_case('etc.', [{ORTH: 'etc.', NORM: 'etc.'}])
+nlp.tokenizer.add_special_case('Mr.', [{ORTH: 'Mr.', NORM: 'Mr.'}])
+nlp.tokenizer.add_special_case('Mrs.', [{ORTH: 'Mrs.', NORM: 'Mrs.'}])
+nlp.tokenizer.add_special_case('Dr.', [{ORTH: 'Dr.', NORM: 'Dr.'}])
 
 # sentence segmentation
 nlp.add_pipe('sentencizer')
@@ -31,9 +38,6 @@ def transform_to_iob(item):
     tokens_spacy = list(nlp(item.text.replace("\n", "").strip(), disable=DISABLED))
     tokens = []
     for token in tokens_spacy:
-        if str(token).isspace():
-            pass
-        else:
             tokens.append(token)
 
     return [
@@ -57,13 +61,17 @@ DATE_counter = 0
 CAMP_counter = 0
 GHETTO_counter = 0
 
-
 for subdir, dirs, files in os.walk(corpus_directory):
     for file in files:
         print(f'Processing {file} in sub-directory {subdir}')
         with open(os.path.join(subdir, file), mode='r', encoding='utf-8') as fh:
             # parsing the XML-TEI file
-            soup = BeautifulSoup(fh, features='xml')
+            # Getting rid of multiple spaces
+            xml_str = fh.read()
+            text=re.sub("\s\s+", " ", xml_str)
+            # Getting rid of space at the beginning of text
+            text = re.sub("^\s", "", text)         
+            soup = BeautifulSoup(text, features='xml')
             # looking for <body>
             for elem in soup.find_all("body"):
                 # by default, we iterate on all <p>s, can be modified depending on the TEI tree
@@ -71,7 +79,6 @@ for subdir, dirs, files in os.walk(corpus_directory):
                     # we iterate on all children of <p>
                     for child in p.children:
                         # same logic is used for remaining entities
-                        
                         if child.name == 'placeName':
                             if child.has_attr('type'):
                                 if child['type'] == 'camp':
@@ -108,11 +115,12 @@ for subdir, dirs, files in os.walk(corpus_directory):
                             label = 'O'
                             doc = nlp(str(child.text).replace("\n", "").strip(), disable=DISABLED)
                             for sentence in doc.sents:
-                                for token in sentence:
-                                    if str(token).isspace():
-                                        pass
-                                    else:
+                                for i, token in enumerate(sentence):
+                                    if i == len(sentence)-1 and token.text == '.' or token.text == '?' or token.text == '!' or token.text == ';' or token.text == ':':
                                         annotations.append([str(token), label]) 
+                                        annotations.append(['nAn'])
+                                    else:
+                                        annotations.append([str(token), label])
 
 # updating the entities count
 stats['entity']['LOC'] += LOC_counter
@@ -123,19 +131,12 @@ stats['entity']['GHETTO'] += GHETTO_counter
 stats['entity']['CAMP'] += CAMP_counter
 stats['token'] += len(annotations)
 
-
-with open(f'../../lang/subset_2/de/ehri-ner_de_s1.txt', mode='w+', encoding='utf-8') as iob_file:
+with open(f'./test.txt', mode='w+', encoding='utf-8') as iob_file:
     for annotation in annotations:
-        if annotation[0] == '.':
-            if annotation[1] == 'I-DATE':
-                iob_file.write(' '.join(annotation) + '\n')
-            else:
-                iob_file.write(' '.join(annotation) + '\n\n')
-        elif annotation[0] == '"':
-            pass
+        if annotation[0] == 'nAn':
+            iob_file.write('\n')
         else:
             iob_file.write(' '.join(annotation)+ '\n')
 
-with open(f'../../lang/subset_2/de/ehri-ner_de_s1_stats.txt', mode='w+', encoding='utf-8') as txt:
+with open(f'./test_stat.txt', mode='w+', encoding='utf-8') as txt:
     txt.write(str(stats))
-    
